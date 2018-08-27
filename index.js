0e1d8a7675125f59552abf4b25dc2f1a567a2d62
Salah Al-Dhaferi <binaryminded@gmail.com>
2018-08-27 14:11:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (array){
+  let newArray = [];
+  count = 0;
+  for (let element of array){
+    if (count <2){
+      newArray.push(element);
+      count++;
+    }
+    
+  }
+  return newArray;
+  
+};
+
+const returnLastTwoDrivers = function (array){
+  let newArray = [];
+  count = array.length;
+  step = 1;
+  for (let element of array){
+    if (step === count || step === count-1){
+      newArray.push(element);
+      
+    }
+    step++;
+  }
+  return newArray;
+  
+};
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(num){
+  return function (multi){
+    return num * multi;
+    
+  };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(array , func){
+  const newFunc = func(array);
+  return newFunc;
+}
+
+
+
+
+
+
+
+
